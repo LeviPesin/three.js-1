08194ec9f54f9fc194c64e52f9dd95cc99c823fb
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-02-04 06:24:10 +1030

    More

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -4,12 +4,10 @@ import path from 'path';
 import * as fs from 'fs/promises';
 
 const idleTime = 9; // 9 seconds - for how long there should be no network requests
-const parseTime = 6; // 6 seconds per megabyte
 
 const port = 1234;
 
 const networkTimeout = 1.5; // 1.5 minutes, set to 0 to disable
-const renderTimeout = 5; // 5 seconds, set to 0 to disable
 
 const numPages = 16; // use 16 browser pages
 
@@ -66,23 +64,9 @@ async function main() {
 
 async function preparePage( page, injection ) {
 
-	/* let page.file, page.pageSize */
+	/* let page.file */
 
 	await page.evaluateOnNewDocument( injection );
-
-	page.on( 'response', async ( response ) => {
-
-		try {
-
-			if ( response.status === 200 ) {
-
-				await response.buffer().then( buffer => page.pageSize += buffer.length );
-
-			}
-
-		} catch {}
-
-	} );
 
 }
 
@@ -111,8 +95,6 @@ async function makeAttempt( pages, cleanPage, file ) {
 
 	try {
 
-		page.pageSize = 0;
-
 		/* Load target page */
 
 		await page.goto( `http://localhost:${ port }/examples/${ file }.html`, {
@@ -128,35 +110,6 @@ async function makeAttempt( pages, cleanPage, file ) {
 			timeout: networkTimeout * 60000,
 			idleTime: idleTime * 1000
 		} );
-
-		await page.evaluate( async ( renderTimeout, parseTime ) => {
-
-			await new Promise( resolve => setTimeout( resolve, parseTime ) );
-
-			/* Resolve render promise */
-
-			window._renderStarted = true;
-
-			await new Promise( function ( resolve, reject ) {
-
-				const renderStart = performance._now();
-
-				const waitingLoop = setInterval( function () {
-
-					const renderTimeoutExceeded = ( renderTimeout > 0 ) && ( performance._now() - renderStart > 1000 * renderTimeout );
-
-					if ( renderTimeoutExceeded || window._renderFinished ) {
-
-						clearInterval( waitingLoop );
-						resolve();
-
-					}
-
-				}, 10 );
-
-			} );
-
-		}, renderTimeout, page.pageSize / 1024 / 1024 * parseTime * 1000 );
 
 	} catch ( e ) {
 
