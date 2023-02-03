8de77e49544482d4fbb60c9ea039742adf32ae4e
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2023-02-04 04:23:54 +1100

    More

--- test/e2e/puppeteer.js
+++ test/e2e/puppeteer.js
@@ -1,7 +1,6 @@
 import puppeteer from 'puppeteer';
 import express from 'express';
 import path from 'path';
-import jimp from 'jimp';
 import * as fs from 'fs/promises';
 
 /* CONFIG VARIABLES START */
@@ -80,22 +79,16 @@ async function main() {
 
 	/* Launch browser */
 
-	const flags = [ '--hide-scrollbars', '--enable-unsafe-webgpu' ];
-	flags.push( '--enable-features=Vulkan', '--use-gl=swiftshader', '--use-angle=swiftshader', '--use-vulkan=swiftshader', '--use-webgpu-adapter=swiftshader' );
-	// if ( process.platform === 'linux' ) flags.push( '--enable-features=Vulkan,UseSkiaRenderer', '--use-vulkan=native', '--disable-vulkan-surface', '--disable-features=VaapiVideoDecoder', '--ignore-gpu-blocklist', '--use-angle=vulkan' );
+	const flags = [ '--hide-scrollbars', '--enable-unsafe-webgpu', '--enable-features=Vulkan', '--use-gl=swiftshader', '--use-angle=swiftshader', '--use-vulkan=swiftshader', '--use-webgpu-adapter=swiftshader' ];
 
 	const viewport = { width: width * viewScale, height: height * viewScale };
 
 	browser = await puppeteer.launch( {
-		headless: ! process.env.VISIBLE,
+		headless: true,
 		args: flags,
 		defaultViewport: viewport,
 		handleSIGINT: false
 	} );
-
-	// this line is intended to stop the script if the browser (in headful mode) is closed by user (while debugging)
-	// browser.on( 'targetdestroyed', target => ( target.type() === 'other' ) ? close() : null );
-	// for some reason it randomly stops the script after about ~30 screenshots processed
 
 	/* Prepare injections */
 
@@ -194,81 +187,55 @@ async function makeAttempt( pages, cleanPage, file ) {
 	try {
 
 		page.pageSize = 0;
-		page.error = undefined;
 
 		/* Load target page */
 
-		try {
+		await page.goto( `http://localhost:${ port }/examples/${ file }.html`, {
+			waitUntil: 'networkidle0',
+			timeout: networkTimeout * 60000
+		} );
 
-			await page.goto( `http://localhost:${ port }/examples/${ file }.html`, {
-				waitUntil: 'networkidle0',
-				timeout: networkTimeout * 60000
+		/* Render page */
+
+		await page.evaluate( cleanPage );
+
+		await page.waitForNetworkIdle( {
+			timeout: networkTimeout * 60000,
+			idleTime: idleTime * 1000
+		} );
+
+		await page.evaluate( async ( renderTimeout, parseTime ) => {
+
+			await new Promise( resolve => setTimeout( resolve, parseTime ) );
+
+			/* Resolve render promise */
+
+			window._renderStarted = true;
+
+			await new Promise( function ( resolve, reject ) {
+
+				const renderStart = performance._now();
+
+				const waitingLoop = setInterval( function () {
+
+					const renderTimeoutExceeded = ( renderTimeout > 0 ) && ( performance._now() - renderStart > 1000 * renderTimeout );
+
+					if ( renderTimeoutExceeded || window._renderFinished ) {
+
+						clearInterval( waitingLoop );
+						resolve();
+
+					}
+
+				}, 10 );
+
 			} );
 
-		} catch ( e ) {
+		}, renderTimeout, page.pageSize / 1024 / 1024 * parseTime * 1000 );
 
-			throw new Error( `Error happened while loading file ${ file }: ${ e }` );
+	} catch ( e ) {
 
-		}
-
-		try {
-
-			/* Render page */
-
-			await page.evaluate( cleanPage );
-
-			await page.waitForNetworkIdle( {
-				timeout: networkTimeout * 60000,
-				idleTime: idleTime * 1000
-			} );
-
-			await page.evaluate( async ( renderTimeout, parseTime ) => {
-
-				await new Promise( resolve => setTimeout( resolve, parseTime ) );
-
-				/* Resolve render promise */
-
-				window._renderStarted = true;
-
-				await new Promise( function ( resolve, reject ) {
-
-					const renderStart = performance._now();
-
-					const waitingLoop = setInterval( function () {
-
-						const renderTimeoutExceeded = ( renderTimeout > 0 ) && ( performance._now() - renderStart > 1000 * renderTimeout );
-
-						if ( renderTimeoutExceeded ) {
-
-							clearInterval( waitingLoop );
-							reject( 'Render timeout exceeded' );
-
-						} else if ( window._renderFinished ) {
-
-							clearInterval( waitingLoop );
-							resolve();
-
-						}
-
-					}, 10 );
-
-				} );
-
-			}, renderTimeout, page.pageSize / 1024 / 1024 * parseTime * 1000 );
-
-		} catch ( e ) {
-
-			if ( ! e.message.includes( 'Render timeout exceeded' ) ) {
-
-				throw new Error( `Error happened while rendering file ${ file }: ${ e }` );
-
-			}
-
-		}
-
-		await jimp.read( await page.screenshot() );
-
-		if ( page.error !== undefined ) throw new Error( page.error );
+		throw e;
 
 	} finally {
 
